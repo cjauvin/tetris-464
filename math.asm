9f970f61a73ebd769225b8cde2deac9b119d174e
Christian Jauvin <cjauvin@gmail.com>
2012-06-21 16:19:45 -0400

    reorg

--- math.asm
+++ math.asm
@@ -1,9 +1,9 @@
-// zeropage free bytes: fb, fc, fd, fe
-        
-// name:	8 bit multiplication, written by Damon Slye
-// call:	accu: multiplier
-//      	x-register: multiplicant
-//return:	product in accu (hibyte) and x-register (lowbyte)
+/*        
+  8 bit multiplication, written by Damon Slye
+  call:	  accu: multiplier
+      	  x-register: multiplicant
+  return:   product in accu (hibyte) and x-register (lowbyte)
+*/
 mult:   cpx #$00
 	beq end
 	dex
@@ -23,7 +23,9 @@ skip:   ror
 end:    txa
 	rts
 
-// 16-bit addition: vw2 = vw0 + vw1
+/*
+   16-bit addition: vw2 = vw0 + vw1
+*/
 add2:   clc
         lda vw0
         adc vw1
@@ -32,8 +34,9 @@ add2:   clc
         adc vw1+1
         sta vw2+1
         rts
-
-// 16-bit addition: vw3 = vw0 + vw1 + vw2
+/*
+   16-bit addition: vw3 = vw0 + vw1 + vw2
+*/
 add3:   clc
         lda vw0
         adc vw1
--- tetris.asm
+++ tetris.asm
@@ -1,21 +1,39 @@
-// test
-:BasicUpstart2(start) // autostart macro
+/*
+   tetris.asm (c=64)
+   June 2012
+   Christian Jauvin
+   cjauvin@gmail.com
+   http://christianjauv.in
+*/
+        
+:BasicUpstart2(main) // autostart macro
 
+.pc = $1000 "Tetrominoes data"
+        // 'O' piece
+        .byte 1 // number of states
+        .byte 0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0
+                
 .pc = $2000 "Variables"
 pos:    .byte 0, 17 // starting pos: top center
 cntr:   .byte 0     // multi-purpose counter
-vb0:    .byte 0 // multi-purpose var (byte)
-vb1:    .byte 0 // var (byte)
-vb2:    .byte 0 // var (byte)
-vb3:    .byte 0 // var (byte)
-vw0:    .byte 0,0 // var (word, i.e. 16 bits)
-vw1:    .byte 0,0 // var (word)
-vw2:    .byte 0,0 // var (word)
-vw3:    .byte 0,0 // var (word)
+vb0:    .byte 0 // multi-purpose vars (byte)
+vb1:    .byte 0 
+vb2:    .byte 0 
+vb3:    .byte 0 
+vw0:    .word 0 // word vars (16 bits)
+vw1:    .word 0 
+vw2:    .word 0 
+vw3:    .word 0 
         
 .import source "math.asm"
+
+///////////////////////////////////////////////////////
         
-grid_side:      ldx #0
+/*
+   Draw left and right grid sides
+*/
+grid_outline_side:
+        ldx #0
         ldy #0
 !loop:  clc         // (($fb) += 40) X 21
         lda $fb,y
@@ -30,58 +48,37 @@ grid_side:      ldx #0
         cpx #21
         bne !loop-
         rts
-        
-start:  jsr $e544   // clear screen
 
-        // grid bottom
-        lda #160    // 32 | 128 (space in rev video)
-        ldx #0
-!loop:  sta $07a6,x // $07a6 = 1024 + (23 * 40) + 14
-        inx
-        cpx #12
-        bne !loop-
-
-        // grid left
-        lda #$36    // $fb -> $0436 = 1024 + (1 * 40) + 14
-        sta $fb
-        lda #$04
-        sta $fc        
-        jsr grid_side
-
-        // grid right
-        lda #$41    // $fb -> $0442 = 1024 + (1 * 40) + 14 + 11
-        sta $fb
-        lda #$04
-        sta $fc        
-        jsr grid_side
-
-        // place 'O' at pos
+/*
+   Draw piece at pos
+*/
+draw_piece:
         lda #0
         sta cntr // counter from 0 to 15
 
         lda #0
-        sta vb0 // x: 0 to 3 (rows)
+        sta vb0 // i: 0 to 3 (piece rows)
 
-row4:       lda #0
-            sta vb1 // y: 0 to 3 (cols)
+prow:       lda #0
+            sta vb1 // j: 0 to 3 (piece cols)
 
-                // target cell address: 1024 + (pos[0] * 40) + pos[1]
-col4:           lda #0 // vw0 <- 1024
-                sta vw1
+                // target cell address: 1024 + (pos[0]+i * 40) + pos[1]+j
+pcol:           lda #0 // vw0 <- 1024
+                sta vw0
                 lda #4
                 sta vw0+1
 
-                lda pos // vw1 <- pos[x] * 40
+                lda pos // vw1 <- pos[0][i] * 40
                 clc
-                adc vb0
+                adc vb0 // i
                 ldx #40
                 jsr mult
                 stx vw1  
                 sta vw1+1
 
-                lda pos+1 // vw2 <- pos[y]
+                lda pos+1 // vw2 <- pos[1][j]
                 clc
-                adc vb1
+                adc vb1 // j
                 sta vw2
                 lda #0
                 sta vw2+1
@@ -107,21 +104,47 @@ off:            ldy #0
                 iny
                 sty vb1
                 cpy #4
-                bne col4
+                bne pcol
 
            ldx vb0
            inx
            stx vb0
            cpx #4
-           bne row4
-                
-        jmp *
+           bne prow
 
-        
-.pc = $1000 "Tetrominoes data"
-        // 'O' piece
-        .byte 1 // number of states
-        .byte 0,0,0,0,0,0,1,1,0,0,1,1,0,0,0,0
-        //.byte 1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1
+        rts
 
+/////////////////////////////////////////////        
+
+main:   jsr $e544   // clear screen
+
+        // draw 3 parts of grid outline:        
+
+        // (1) bottom
+        lda #160    // 32 | 128 (space in rev video)
+        ldx #0
+!loop:  sta $07a6,x // $07a6 = 1024 + (23 * 40) + 14
+        inx
+        cpx #12
+        bne !loop-
+
+        // (2) left
+        lda #$36    // $fb -> $0436 = 1024 + (1 * 40) + 14
+        sta $fb
+        lda #$04
+        sta $fc        
+        jsr grid_outline_side
+
+        // (3) right
+        lda #$41    // $fb -> $0442 = 1024 + (1 * 40) + 14 + 11
+        sta $fb
+        lda #$04
+        sta $fc        
+        jsr grid_outline_side
+
+        // start
         
+        jsr draw_piece
+
+        jmp * // infinite loop
+
