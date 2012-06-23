5de7e074cabb7a0072849e1d0dc7456f84b0fa0e
Christian Jauvin <cjauvin@gmail.com>
2012-06-23 09:04:51 -0400

    everything in handler

--- tetris.asm
+++ tetris.asm
@@ -9,18 +9,22 @@
         
 :BasicUpstart2(main) // autostart macro
 
+.import source "math.asm"
+
 .pc = $1000 "Tetrominoes data"
         // 'O' piece
         .byte 1 // number of states
-        //.byte 0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0
-        .byte 1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1
+        .byte 0,0,0,0,0,1,1,0,0,1,1,0,0,0,0,0
+        //.byte 1,0,1,0,0,1,0,1,1,0,1,0,0,1,0,1
         //.byte 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1
                 
 .pc = $2000 "Variables"
+fixed: .fill 1000, 32
 pos:    .byte 0, 18 // starting pos: top center
+pos_ahead: .word 0
 cntr:   .byte 0     // multi-purpose counter
-timer1: .byte 0, 60     // val, target
-timer2: .byte 0, 10
+timer1: .byte 0, 30     // val, target
+timer2: .byte 0, 5
 vb0:    .byte 0     // multi-purpose vars (byte)
 vb1:    .byte 0 
 vb2:    .byte 0 
@@ -28,10 +32,8 @@ vb3:    .byte 0
 vw0:    .word 0     // word vars (16 bits)
 vw1:    .word 0 
 vw2:    .word 0 
-vw3:    .word 0 
+vw3:    .word 0
         
-.import source "math.asm"
-
 ///////////////////////////////////////////////////////
 
 /*
@@ -63,6 +65,9 @@ keyboard_input:
 test_left:      
         cmp #$41      // 'A' key
         bne test_right
+        lda #1
+        jsr can_move // can move left?
+        //bne test_right
         lda #0
         jsr draw_piece
         dec pos+1       // piece left
@@ -71,6 +76,9 @@ test_left:
 test_right:
         cmp #$44      // 'D' key
         bne !return+
+        lda #2
+        jsr can_move // can move right?
+        //bne test_right
         lda #0
         jsr draw_piece
         inc pos+1       // piece right
@@ -80,7 +88,7 @@ test_right:
 !wait:
         inc timer2
 !return:
-        jmp $ea31   
+        jmp $ea31 // $ea31=full, $ea81=pop registers only 
         
 /*
    Draw left and right grid sides
@@ -103,6 +111,39 @@ grid_outline_side:
         rts
 
 /*
+   
+*/
+get_cell_addr:
+        lda #0 // vw0 <- 1024
+        sta vw0
+        lda #4
+        sta vw0+1
+
+        lda pos // vw1 <- pos[0][i] * 40
+        clc
+        adc vb1 // i
+        ldx #40
+        jsr mult
+        stx vw1  
+        sta vw1+1
+
+        lda pos+1 // vw2 <- pos[1][j]
+        clc
+        adc vb2 // j
+        sta vw2
+        lda #0
+        sta vw2+1
+
+        jsr add3 // vw3 = vw0 + vw1 + vw2
+
+        lda vw3
+        sta $fb
+        lda vw3+1
+        sta $fc
+
+        rts
+        
+/*
    Draw piece at pos
    acc=1 -> draw
    acc=0 -> erase
@@ -121,33 +162,9 @@ draw_piece:
             sta vb2 // j: 0 to 3 (piece cols)
 
                 // target cell address: 1024 + (pos[0]+i * 40) + pos[1]+j
-!pcol:          lda #0 // vw0 <- 1024
-                sta vw0
-                lda #4
-                sta vw0+1
-
-                lda pos // vw1 <- pos[0][i] * 40
-                clc
-                adc vb1 // i
-                ldx #40
-                jsr mult
-                stx vw1  
-                sta vw1+1
-
-                lda pos+1 // vw2 <- pos[1][j]
-                clc
-                adc vb2 // j
-                sta vw2
-                lda #0
-                sta vw2+1
-
-                jsr add3 // vw3 = vw0 + vw1 + vw2
+!pcol:
+                jsr get_cell_addr
         
-                lda vw3
-                sta $fb
-                lda vw3+1
-                sta $fc
-
                 lda vb0
                 beq erase
 draw:   
@@ -181,7 +198,115 @@ erase:
            bne !prow-
 
         rts
+
+/*
+   Input:  
+      acc=0: down, acc=1: left, acc=2: right
+   Output:
+      acc=0: no, acc=1: yes
+*/
+can_move:
+
+        ldx pos
+        ldy pos+1
+        stx pos_ahead
+        sty pos_ahead+1
+
+        cmp #0
+        beq down
+        cmp #1
+        beq left
+        cmp #2
+        beq right
+
+down:   inc pos_ahead
+        jmp !continue+
+left:   dec pos_ahead+1
+        jmp !continue+
+right:  inc pos_ahead+1
+        jmp !continue+
+       
+!continue:
+
+        lda #0
+        sta cntr // counter from 0 to 15
+
+        lda #0
+        sta vb1 // i: 0 to 3 (piece rows)
+
+!prow:      lda #0
+            sta vb2 // j: 0 to 3 (piece cols)
+
+                // target cell address: fixed + (pos_ahead[0]+i * 40) + pos_ahead[1]+j
+!pcol:        
+                lda #<fixed // vw0 <- fixed
+                sta vw0
+                lda #>fixed
+                sta vw0+1
+
+
+                lda pos_ahead // vw1 <- pos_ahead[0][i] * 40
+                clc
+                adc vb1 // i
+                ldx #40
+
+                lda #1
+                sta vb3
+!loop:
+                lda vb3
+                cmp #255
+                beq !continue+
+                inc vb3
+                jmp !loop-
         
+                //jsr mult
+                //stx vw1  
+                //sta vw1+1
+
+/*        
+
+                lda pos_ahead+1 // vw2 <- pos_ahead[1][j]
+                clc
+                adc vb2 // j
+                sta vw2
+                lda #0
+                sta vw2+1
+
+                jsr add3 // vw3 = vw0 + vw1 + vw2
+
+                lda vw3
+                sta $fb
+                lda vw3+1
+                sta $fc
+
+                ldy #0
+                lda ($fb),y
+                cmp #32
+                beq !continue+
+                ldy cntr
+                ldx $1001,y
+                beq !continue+
+                lda #1 // collision detected
+                rts
+*/
+
+!continue:
+                inc cntr        
+                ldy vb2
+                iny
+                sty vb2
+                cpy #4
+                bne !pcol-
+
+           ldx vb1
+           inx
+           stx vb1
+           cpx #4
+           bne !prow-
+
+        lda #0 // no collision found
+        rts
+                
 /////////////////////////////////////////////        
 
 main:
@@ -189,13 +314,14 @@ main:
 
         lda #128
         sta $028a // set key autorepeat
-        
+                
         // draw 3 parts of grid outline:        
 
-        // (1) bottom
+        // (1) bottom        
         lda #160    // 32 | 128 (space in rev video)
         ldx #0
 !loop:  sta $07a6,x // $07a6 = 1024 + (23 * 40) + 14
+        sta $23a6,x // $23a6 = 8192 + (23 * 40) + 14
         inx
         cpx #12
         bne !loop-
@@ -207,6 +333,12 @@ main:
         sta $fc        
         jsr grid_outline_side
 
+        lda #$36    // $fb -> $2036 = 8192 + (1 * 40) + 14
+        sta $fb
+        lda #$20
+        sta $fc        
+        jsr grid_outline_side
+        
         // (3) right
         lda #$41    // $fb -> $0442 = 1024 + (1 * 40) + 14 + 11
         sta $fb
